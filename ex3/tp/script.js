ff158a59c1f97f7e1b2e93a85e590c1b76359e2f
DavithCH <chhungdavith@gmail.com>
2022-02-04 17:38:26 +0100

    just commit

--- ex3/tp/script.js
+++ ex3/tp/script.js
@@ -9,17 +9,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 let userData;
 let postData;
+let data;
 const userBlog = document.querySelector(".user-blog");
 const fetchData = () => __awaiter(this, void 0, void 0, function* () {
     const reqUser = yield fetch("https://jsonplaceholder.typicode.com/users");
     userData = yield reqUser.json();
     const reqPost = yield fetch("https://jsonplaceholder.typicode.com/posts");
     postData = yield reqPost.json();
-    const data = userData.map((u) => {
+    data = userData.map((u) => {
         return Object.assign({}, u, postData.filter((p) => p.userId === u.id));
     });
     let html = "";
-    console.log(data);
     for (let i = 0; i < data.length; i++) {
         html += `
               <h2>${data[i].name}</h2>
@@ -34,7 +34,66 @@ const fetchData = () => __awaiter(this, void 0, void 0, function* () {
         });
     }
     userBlog.innerHTML = html;
+    filter(data);
 });
+const filter = (data) => {
+    const form = document.querySelector("form");
+    const byTitle = document.querySelector(".by-title");
+    const byUser = document.querySelector(".by-user");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        if (byUser.value !== "") {
+            data = userData.filter((u) => {
+                if (u.name === byUser.value) {
+                    // return Object.assign(
+                    //   {},
+                    //   u,
+                    //   postData.filter((p) => p.userId === u.id)
+                    // );
+                    return [u, postData.filter((p) => p.userId === u.id)];
+                }
+            });
+            console.log(data);
+            let html = "";
+            for (let i = 0; i < data.length; i++) {
+                html += `
+              <h2>${data[i].name}</h2>
+              <p>${data[i].email}</p>
+              <h1>Titre des articles rédigés</h1>
+              <ul>
+            `;
+                data.map((d) => {
+                    if (d[i].title) {
+                        html += `<li>
+        ${d[i].title}
+      </li></ul>`;
+                    }
+                });
+            }
+            userBlog.innerHTML = html;
+        }
+        if (byTitle.value !== "") {
+            data = postData.filter((p) => {
+                p.title === byTitle.value &&
+                    Object.assign({}, p, userData.filter((u) => u.id === p.userId));
+            });
+            let html = "";
+            for (let i = 0; i < data.length; i++) {
+                html += `
+              <h2>${data[i].name}</h2>
+              <p>${data[i].email}</p>
+              <h1>Titre des articles rédigés</h1>
+              <ul>
+            `;
+                data.map((d) => {
+                    html += `<li>
+        ${d[i].title}
+      </li></ul>`;
+                });
+            }
+            userBlog.innerHTML = html;
+        }
+    });
+};
 fetchData();
-//  tsc script.ts && node script.js
 // tsc script.ts --lib "ES2015","DOM" --watch --target es6 && node script.js
--- ex3/tp/script.ts
+++ ex3/tp/script.ts
@@ -1,5 +1,6 @@
-let userData;
-let postData;
+let userData: Array<any>;
+let postData: Array<any>;
+let data;
 
 const userBlog = document.querySelector(".user-blog") as HTMLDivElement;
 
@@ -10,16 +11,14 @@ const fetchData = async () => {
   const reqPost = await fetch("https://jsonplaceholder.typicode.com/posts");
   postData = await reqPost.json();
 
-  const data = userData.map((u) => {
+  data = userData.map((u) => {
     return Object.assign(
       {},
       u,
-      postData.filter((p) => p.userId === u.id)
+      postData.filter((p: any) => p.userId === u.id)
     );
   });
   let html: string = "";
-
-  console.log(data);
 
   for (let i = 0; i < data.length; i++) {
     html += `
@@ -35,9 +34,80 @@ const fetchData = async () => {
     });
   }
   userBlog.innerHTML = html;
+
+  filter(data);
+};
+
+const filter = (data: any) => {
+  const form = document.querySelector("form") as HTMLFormElement;
+  const byTitle = document.querySelector(".by-title") as HTMLInputElement;
+  const byUser = document.querySelector(".by-user") as HTMLInputElement;
+
+  form.addEventListener("submit", (e: Event) => {
+    e.preventDefault();
+
+    if (byUser.value !== "") {
+      data = userData.filter((u) => {
+        if (u.name === byUser.value) {
+          // return Object.assign(
+          //   {},
+          //   u,
+          //   postData.filter((p) => p.userId === u.id)
+          // );
+          return [u, postData.filter((p) => p.userId === u.id)];
+        }
+      });
+
+      console.log(data);
+      let html: string = "";
+
+      for (let i = 0; i < data.length; i++) {
+        html += `
+              <h2>${data[i].name}</h2>
+              <p>${data[i].email}</p>
+              <h1>Titre des articles rédigés</h1>
+              <ul>
+            `;
+
+        data.map((d: any) => {
+          if (d[i].title) {
+            html += `<li>
+        ${d[i].title}
+      </li></ul>`;
+          }
+        });
+      }
+      userBlog.innerHTML = html;
+    }
+
+    if (byTitle.value !== "") {
+      data = postData.filter((p) => {
+        p.title === byTitle.value &&
+          Object.assign(
+            {},
+            p,
+            userData.filter((u) => u.id === p.userId)
+          );
+      });
+      let html: string = "";
+
+      for (let i = 0; i < data.length; i++) {
+        html += `
+              <h2>${data[i].name}</h2>
+              <p>${data[i].email}</p>
+              <h1>Titre des articles rédigés</h1>
+              <ul>
+            `;
+        data.map((d) => {
+          html += `<li>
+        ${d[i].title}
+      </li></ul>`;
+        });
+      }
+      userBlog.innerHTML = html;
+    }
+  });
 };
 fetchData();
-
-//  tsc script.ts && node script.js
 
 // tsc script.ts --lib "ES2015","DOM" --watch --target es6 && node script.js
