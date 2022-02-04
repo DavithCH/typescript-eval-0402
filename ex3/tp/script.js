396e0fdbbd57f77bcfbb374d7b6d51456cc4cc89
DavithCH <chhungdavith@gmail.com>
2022-02-04 16:56:03 +0100

    finish 1 part of ex3

--- ex3/tp/script.js
+++ ex3/tp/script.js
@@ -7,47 +7,34 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var _this = this;
-var fetchUsers = function () { return __awaiter(_this, void 0, void 0, function () {
-    var response, data;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, fetch("https://jsonplaceholder.typicode.com/users")];
-            case 1:
-                response = _a.sent();
-                return [4 /*yield*/, response.json()];
-            case 2:
-                data = _a.sent();
-                console.log(data);
-                return [2 /*return*/];
-        }
+let userData;
+let postData;
+const userBlog = document.querySelector(".user-blog");
+const fetchData = () => __awaiter(this, void 0, void 0, function* () {
+    const reqUser = yield fetch("https://jsonplaceholder.typicode.com/users");
+    userData = yield reqUser.json();
+    const reqPost = yield fetch("https://jsonplaceholder.typicode.com/posts");
+    postData = yield reqPost.json();
+    const data = userData.map((u) => {
+        return Object.assign({}, u, postData.filter((p) => p.userId === u.id));
     });
-}); };
-fetchUsers();
+    let html = "";
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+        html += `
+              <h2>${data[i].name}</h2>
+              <p>${data[i].email}</p>
+              <h1>Titre des articles rédigés</h1>
+              <ul>
+            `;
+        data.map((d) => {
+            html += `<li>
+        ${d[i].title}
+      </li></ul>`;
+        });
+    }
+    userBlog.innerHTML = html;
+});
+fetchData();
+//  tsc script.ts && node script.js
+// tsc script.ts --lib "ES2015","DOM" --watch --target es6 && node script.js
--- ex3/tp/script.ts
+++ ex3/tp/script.ts
@@ -1,7 +1,43 @@
-const fetchUsers = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await response.json();
-  console.log(data);
-};
+let userData;
+let postData;
 
-fetchUsers();
+const userBlog = document.querySelector(".user-blog") as HTMLDivElement;
+
+const fetchData = async () => {
+  const reqUser = await fetch("https://jsonplaceholder.typicode.com/users");
+  userData = await reqUser.json();
+
+  const reqPost = await fetch("https://jsonplaceholder.typicode.com/posts");
+  postData = await reqPost.json();
+
+  const data = userData.map((u) => {
+    return Object.assign(
+      {},
+      u,
+      postData.filter((p) => p.userId === u.id)
+    );
+  });
+  let html: string = "";
+
+  console.log(data);
+
+  for (let i = 0; i < data.length; i++) {
+    html += `
+              <h2>${data[i].name}</h2>
+              <p>${data[i].email}</p>
+              <h1>Titre des articles rédigés</h1>
+              <ul>
+            `;
+    data.map((d) => {
+      html += `<li>
+        ${d[i].title}
+      </li></ul>`;
+    });
+  }
+  userBlog.innerHTML = html;
+};
+fetchData();
+
+//  tsc script.ts && node script.js
+
+// tsc script.ts --lib "ES2015","DOM" --watch --target es6 && node script.js
